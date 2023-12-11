e7c0887a4aa3db8bc5ef33775e157abcb8f03a5b
jonasw247 <jonas.weidner@outlook.de>
2023-12-11 11:30:47 +0100

    switch to function

--- main_LMI_inference.py
+++ main_LMI_inference.py
@@ -7,73 +7,68 @@ import os
 from scipy import ndimage
 from simulator import runForwardSolver
 
-atlasPath = "./Atlasfiles"
+
+#%% run LMI WM is needed for registration
+def runLMI(patientWM, patientFlair, patientT1):
+    atlasPath = "./Atlasfiles"
+
+    wmTransformed, transformedTumor, registration = tools.getAtlasSpaceLMI_InputArray(patientWM, patientFlair, patientT1, atlasPath, getAlsoWMTrafo=True)
+
+    #%% get the LMI prediction
+    prediction = np.array(tools.getNetworkPrediction(transformedTumor))[:6]
+
+    #%% plot the prediction
+    D = prediction[0]
+    rho = prediction[1]
+    T = prediction[2]
+    x = prediction[3]
+    y = prediction[4]
+    z = prediction[5]
+
+    parameterDir = {'D': prediction[0], 'rho': prediction[1], 'T': prediction[2], 'x': prediction[3], 'y': prediction[4], 'z': prediction[5]}
+
+    #%% run model with the given parameters
+    brainPath = os.path.abspath('./simulator/brain')
+    absPath = os.path.abspath(atlasPath + '/anatomy_dat/') + '/' # the "/"c is crucial for the solver
+    tumor = runForwardSolver.run(absPath, prediction, brainPath)
+    np.save('tumor.npy', tumor)
+
+    # %% register back to patient space
+
+    predictedTumorPatientSpace = tools.convertTumorToPatientSpace(tumor, patientWM, registration)
+
+    return predictedTumorPatientSpace, parameterDir
+
+if __name__ == "__main__":
+    patientNumber = 1
+
+    print("patient number: ", patientNumber)
+    patientPath = "/mnt/8tb_slot8/jonas/datasets/MichalsGlioblastomaDATA/GlioblastomaDATA/P" + str(patientNumber)+ "/P"+str(patientNumber)+"/"
+
+    #set the paths to the wm, flair and t1c segmentations
+    wmSegmentationNiiPath = patientPath + 'wm.nii'
+    flairSegmentationNiiPath = patientPath + 'tumor_segm_FLAIR.nii'
+    t1SegmentationNiiPath = patientPath + 'tumor_segm_T1Gd.nii'
+
+    resultPath = 'resultsTGM/' + str(patientNumber) + '/'
+
+    os.makedirs(resultPath, exist_ok=True)
+    patientWMNib = nib.load(wmSegmentationNiiPath)
+
+    patientFlair = nib.load(flairSegmentationNiiPath).get_fdata()
+    if len(patientFlair.shape) == 4:
+        patientFlair = patientFlair[:,:,:,0]
+    patientT1 = nib.load(t1SegmentationNiiPath).get_fdata()
+    if len(patientT1.shape) == 4:
+        patientT1 = patientT1[:,:,:,0]
 
 
-############################################################
-###################### settings start ######################
+    patientWM = patientWMNib.get_fdata()	
+    patientWMAffine = patientWMNib.affine
 
-patientNumber = 1
-print("patient number: ", patientNumber)
-patientPath = "/mnt/8tb_slot8/jonas/datasets/MichalsGlioblastomaDATA/GlioblastomaDATA/P" + str(patientNumber)+ "/P"+str(patientNumber)+"/"
 
-#set the paths to the wm, flair and t1c segmentations
-wmSegmentationNiiPath = patientPath + 'wm.nii'
-flairSegmentationNiiPath = patientPath + 'tumor_segm_FLAIR.nii'
-t1SegmentationNiiPath = patientPath + 'tumor_segm_T1Gd.nii'
+    predictedTumorPatientSpace, parameterDir = runLMI(patientWM, patientFlair, patientT1)
 
-resultPath = 'results/' + str(patientNumber) + '/'
-###################### settings end #########################
-#############################################################
+    np.save(resultPath+'lmi_parameters.npy', parameterDir)
 
-os.makedirs(resultPath, exist_ok=True)
-patientWMNib = nib.load(wmSegmentationNiiPath)
-
-patientFlair = nib.load(flairSegmentationNiiPath).get_fdata()
-if len(patientFlair.shape) == 4:
-    patientFlair = patientFlair[:,:,:,0]
-patientT1 = nib.load(t1SegmentationNiiPath).get_fdata()
-if len(patientT1.shape) == 4:
-    patientT1 = patientT1[:,:,:,0]
-
-#%% register to correct 128 file
-patientWM = patientWMNib.get_fdata()	
-patientWMAffine = patientWMNib.affine
-
-wmTransformed, transformedTumor, registration = tools.getAtlasSpaceLMI_InputArray(patientWM, patientFlair, patientT1, atlasPath, getAlsoWMTrafo=True)
-
-#%% get the LMI prediction
-prediction = np.array(tools.getNetworkPrediction(transformedTumor))[:6]
-
-#%% plot the prediction
-D = prediction[0]
-rho = prediction[1]
-T = prediction[2]
-x = prediction[3]
-y = prediction[4]
-z = prediction[5]
-
-parameterDir = {'D': prediction[0], 'rho': prediction[1], 'T': prediction[2], 'x': prediction[3], 'y': prediction[4], 'z': prediction[5]}
-np.save(resultPath+'lmi_parameters.npy', parameterDir)
-
-#plt.imshow(transformedTumor[:,:,int(z*129)], cmap='Reds', alpha=0.6)
-imageAtlas = np.load(atlasPath+'/anatomy/npzstuffData_0001.npz')["data"][:,:,:,2]
-plt.imshow(imageAtlas[:,:,int(z*129)], cmap='Greys', alpha=0.6)
-
-plt.imshow(transformedTumor[:,:,int(z*129)], cmap='Reds', alpha=0.6)
-plt.scatter(y*129, x*129, c='r')
-
-#%% run model with the given parameters
-brainPath = os.path.abspath('./simulator/brain')
-absPath = os.path.abspath(atlasPath + '/anatomy_dat/') + '/' # the "/"c is crucial for the solver
-tumor = runForwardSolver.run(absPath, prediction, brainPath)
-np.save('tumor.npy', tumor)
-#%%
-#tumor = np.load('tumor.npy')
-
-# %% register back to patient space
-
-predictedTumorPatientSpace = tools.convertTumorToPatientSpace(tumor, patientWM, registration)
-#%% save
-
-nib.save(nib.Nifti1Image(predictedTumorPatientSpace, patientWMAffine), resultPath+'lmi_tumor_patientSpace.nii')
+    nib.save(nib.Nifti1Image(predictedTumorPatientSpace, patientWMAffine), resultPath+'lmi_tumor_patientSpace.nii')
