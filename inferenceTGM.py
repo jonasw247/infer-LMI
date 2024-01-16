5b3bc90e190668ac166dbe417eecf60e16b73fa0
jonasw247 <jonas.weidner@outlook.de>
2024-01-16 16:53:07 +0100

    old stuff, dont know

--- inferenceTGM.py
+++ inferenceTGM.py
@@ -8,71 +8,82 @@ import ants
 import time
 
 
-start_time = time.time()
+def runForPatient(pat):
 
-#%% save the parameters and the tumor
-patientNumber = "001"
-print("patient number: ", patientNumber)
+    start_time = time.time()
 
-patientPath = "/mnt/8tb_slot8/jonas/datasets/TGM/tgm/tgm"+ patientNumber+"/preop/"
-resultPath = '/mnt/8tb_slot8/jonas/datasets/TGM/resultsLMI-infer/' + str(patientNumber) + '/'
-os.makedirs(resultPath, exist_ok=True)
+    #%% save the parameters and the tumor
+    patientNumber = ("000000" + str(pat))[-3:]
+    #patientNumber = "003"
+    print("patient number: ", patientNumber)
 
-
-#%% read segmentation
-segmentationNiiPath = patientPath + "sub-tgm"+ patientNumber+"_ses-preop_space-sri_seg.nii.gz"
-
-segmentation = nib.load(segmentationNiiPath).get_fdata()
-
-necrotic = segmentation == 4
-enhancing = segmentation == 1
-
-patientFlair = segmentation == 2
-patientT1 = 1.0 * necrotic + 1.0 *enhancing
-
-#%% generate WM mask
-t1IMGPath = patientPath + "sub-tgm"+ patientNumber+"_ses-preop_space-sri_t1.nii.gz"
-t1Array = nib.load(t1IMGPath).get_fdata()
-patientWMAffine = nib.load(t1IMGPath).affine
-
-brainmask = t1Array > 0
-
-img = ants.from_numpy(t1Array)
-
-mask = ants.from_numpy((brainmask * 1.0))# - (segmentation > 0))
-
-print("- start tissue segmentation")
-atropos = ants.atropos(a=img, m = '[0.2,1x1x1]', c = '[5,0]', i='kmeans[3]', x=mask)
-print("- end tissue segmentation")
-#%%
-print("unique",np.unique(segmentation))
-
-patientGM = atropos['probabilityimages'][1].numpy()
-patientWM = atropos['probabilityimages'][2].numpy() 
-patientWM[segmentation >0] = 1
-patientGM[segmentation >0] = 0
-
-patientWM_GM  = 0.2 * patientWM + 0.1 * patientGM 
-
-#%%
-plt.imshow(patientWM[:,:,80])
-nib.save(nib.Nifti1Image(patientWM, patientWMAffine), resultPath+'_wm.nii.gz')
-nib.save(nib.Nifti1Image(patientGM, patientWMAffine), resultPath+'_gm.nii.gz')
-#%%
+    patientPath = "/mnt/8tb_slot8/jonas/datasets/TGM/tgm/tgm"+ patientNumber+"/preop/"
+    resultPath = '/mnt/8tb_slot8/jonas/datasets/TGM/resultsLMI-infer/' + str(patientNumber) + '/'
 
 
-predictedTumorPatientSpace, parameterDir, wmBackTransformed = main_LMI_inference.runLMI(patientWM_GM, 1.0 *patientFlair, 1.0* patientT1,registrationMode = "WM_GM")
+    #%% read segmentation
+    segmentationNiiPath = patientPath + "sub-tgm"+ patientNumber+"_ses-preop_space-sri_seg.nii.gz"
 
-end_time = time.time()
+    segmentation = nib.load(segmentationNiiPath).get_fdata()
 
-execution_time_in_minutes = (end_time - start_time) / 60
+    necrotic = segmentation == 4
+    enhancing = segmentation == 1
 
-print(f"The execution time in minutes is {execution_time_in_minutes}")
+    patientFlair = segmentation == 2
+    patientT1 = 1.0 * necrotic + 1.0 *enhancing
 
-parameterDir["runtime"] = execution_time_in_minutes
-np.save(resultPath+'lmi_parameters.npy', parameterDir)
+    #%% generate WM mask
+    t1IMGPath = patientPath + "sub-tgm"+ patientNumber+"_ses-preop_space-sri_t1.nii.gz"
+    t1Array = nib.load(t1IMGPath).get_fdata()
+    patientWMAffine = nib.load(t1IMGPath).affine
 
-nib.save(nib.Nifti1Image(predictedTumorPatientSpace, patientWMAffine), resultPath+'lmi_tumor_patientSpace.nii')
-nib.save(nib.Nifti1Image(wmBackTransformed, patientWMAffine), resultPath+'lmi_wm_patientSpace.nii')
+    brainmask = t1Array > 0
+
+    img = ants.from_numpy(t1Array)
+
+    mask = ants.from_numpy((brainmask * 1.0))# - (segmentation > 0))
+
+    print("- start tissue segmentation")
+    atropos = ants.atropos(a=img, m = '[0.2,1x1x1]', c = '[5,0]', i='kmeans[3]', x=mask)
+    print("- end tissue segmentation")
+    #%%
+    print("unique",np.unique(segmentation))
+
+    patientGM = atropos['probabilityimages'][1].numpy()
+    patientWM = atropos['probabilityimages'][2].numpy() 
+    patientWM[segmentation >0] = 1
+    patientGM[segmentation >0] = 0
+
+    patientWM_GM  = 0.2 * patientWM + 0.1 * patientGM 
+
+    #%%
+    os.makedirs(resultPath, exist_ok=True)
+
+    plt.imshow(patientWM_GM[:,:,80])
+    nib.save(nib.Nifti1Image(patientWM, patientWMAffine), resultPath+'_wm.nii.gz')
+    nib.save(nib.Nifti1Image(patientGM, patientWMAffine), resultPath+'_gm.nii.gz')
+    #%%
+
+
+    predictedTumorPatientSpace, parameterDir, wmBackTransformed = main_LMI_inference.runLMI(patientWM_GM, 1.0 *patientFlair, 1.0* patientT1,registrationMode = "WM_GM")
+
+    end_time = time.time()
+
+    execution_time_in_minutes = (end_time - start_time) / 60
+
+    print(f"The execution time in minutes is {execution_time_in_minutes}")
+
+    parameterDir["runtime"] = execution_time_in_minutes
+    np.save(resultPath+'lmi_parameters.npy', parameterDir)
+
+    nib.save(nib.Nifti1Image(predictedTumorPatientSpace, patientWMAffine), resultPath+'lmi_tumor_patientSpace.nii')
+    nib.save(nib.Nifti1Image(wmBackTransformed, patientWMAffine), resultPath+'lmi_wm_patientSpace.nii')
 
 # %%
+for i in range(40, 100):
+    try:
+        runForPatient(i)
+
+    except Exception as e:
+        print("error for patient ", i)
+        print(e)
